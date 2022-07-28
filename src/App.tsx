d370c16d6c1809e32965e72474686bbcedeb9e78
Михаил <misha1234567890m@gmail.com>
2022-07-28 18:16:32 +0300

    Верстка

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Nav from "./components/Nav/Nav";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+    
+      <div className="container">
+        
+        <Nav />
+        <div className="header">
+          <div className="main">
+            <h1>NIKE AIR MAX 95</h1>
+            <h5>€417</h5>
+          </div>
+          <p>
+            Taking inspiration from the human body and '90s athletics
+            aesthetics, the Air Max 95 mixes unbelievable comfort with
+            fast-paced style. The wavy side panels add natural flow to any
+            outfit, while visible Nike Air in the heel and forefoot delivers
+            performance comfort.
+          </p>
+          <button>SEE DETAILS</button>
+        
+        </div>
+        
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Nav.scss"
+
+const Nav = () => {
+  return (
+    <div className="lol" >
+      <img src="/img/logo.png" alt="" />
+      
+        <ul className="list">
+          <li>Contacts</li>
+          <li>Catalog</li>
+          <li>Home</li>
+        </ul>
+      
+    </div>
+  );
+};
+
+export default Nav;
